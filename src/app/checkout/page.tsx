d61483840bb28d5efe2480c064fa55a7293edae7
Mohammed Saleh <mdsaleh23@gmail.com>
2024-11-26 14:22:32 +0530

    Add complete project 15

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -3,6 +3,7 @@ import Container from "../components/Container";
 import FormWrap from "../FormWrap";
 import CheckOutClient from "./CheckOutClient";
 
+export const dynamic = 'force-dynamic';
 const CheckOut = () => {
     return ( 
         <div className="p-8">
