6ac305358822eb86bdad47b23cdda52f933c0166
Mohammed Saleh <mdsaleh23@gmail.com>
2024-11-26 12:07:10 +0530

    Add complete project 8

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
     images: {
         domains: ["m.media-amazon.com", "lh3.googleusercontent.com", "firebasestorage.googleapis.com"]
         
-    }
+    },
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
 export default nextConfig;
